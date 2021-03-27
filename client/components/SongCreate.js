c7af0be7d88f52c4affc6633da44e8126adcfe57
sirmcclay <mcclay.design@gmail.com>
2021-03-27 16:11:22 -0300

    config the form elements and input for song

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
 class SongCreate extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { title: '' };
+	}
+
 	render() {
 		return (
 			<div>
 				<h3>Create a New Song</h3>
-				<form></form>
+				<form>
+					<label htmlFor="title">Song Title:</label>
+					<input
+						onChange={(event) => this.setState({ title: event.target.value })}
+						value={this.state.title}
+						type="text"
+						name="title"
+					/>
+				</form>
 			</div>
 		);
 	}
