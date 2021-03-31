3e91004dcd18b1471bdd7411cb41ea3b3fa4618e
sirmcclay <mcclay.design@gmail.com>
2021-03-31 10:21:21 -0300

    executing the mutation

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 class LyricList extends Component {
 	onLike(id) {
-		console.log(id);
+		this.props.mutate({ variables: { id } });
 	}
 
 	renderLyrics() {
@@ -23,4 +25,13 @@ class LyricList extends Component {
 	}
 }
 
-export default LyricList;
+const mutation = gql`
+	mutation LikeLyric($id: ID) {
+		likeLyric(id: $id) {
+			id
+			likes
+		}
+	}
+`;
+
+export default graphql(mutation)(LyricList);
